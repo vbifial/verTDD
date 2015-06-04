4a8521f748ae3acc2e684d0b6cd9b9cbaa66aa99
Vbifial <vbifial@gmail.com>
2015-06-04 20:36:03 +0600

    Added simple test for non-presence.

--- test/TCTester.java
+++ test/TCTester.java
@@ -1,7 +1,31 @@
 package test;
 
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.util.Random;
+
+import terap.TControl;
 import junit.framework.TestCase;
 
 public class TCTester extends TestCase {
+
+	
+	BufferedReader in;
+	PrintWriter out;
+	TControl m;
+	Random r;
+	
+	@Override
+	protected void setUp() throws Exception {
+		m = new TControl();
+		r = new Random();
+	}
+	
+	public void testCheckFactorNonPresenceSimple() {
+		String factor = "illness";
+		m.checkFactor(factor);
+		String checking = "huba-buba";
+		assertFalse(m.isFactorOld(checking));
+	}
 	
 }
