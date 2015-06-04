b02cb252b545070dd4519e51ab74853f03499a92
Vbifial <vbifial@gmail.com>
2015-06-04 20:59:01 +0600

    Added random test for factor presence.

--- test/TCTester.java
+++ test/TCTester.java
@@ -34,4 +34,20 @@ public class TCTester extends TestCase {
 		assertTrue(m.isFactorOld(factor));
 	}
 	
+	public void testCheckFactorPresenceRandom() {
+		int n = r.nextInt(100) + 1;
+		String factors[] = new String[n];
+		for (int i = 0; i < n; i++) {
+			int m = r.nextInt(100) + 1;
+			StringBuilder factor = new StringBuilder();
+			for (int j = 0; j < m; j++) 
+				factor.append((char)('a' + r.nextInt(26)));
+			factors[i] = factor.toString();
+			this.m.checkFactor(factors[i]);
+		}
+		for (int i = 0; i < n; i++) {
+			assertTrue(m.isFactorOld(factors[i]));
+		}
+	}
+	
 }
