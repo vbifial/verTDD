a37156d76204f4dc51522939981d004d0116a161
Vbifial <vbifial@gmail.com>
2015-06-04 21:04:28 +0600

    Added random test for factor non-presence.

--- test/TCTester.java
+++ test/TCTester.java
@@ -3,6 +3,7 @@ package test;
 import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.util.Random;
+import java.util.TreeSet;
 
 import terap.TControl;
 import junit.framework.TestCase;
@@ -47,6 +48,30 @@ public class TCTester extends TestCase {
 		}
 		for (int i = 0; i < n; i++) {
 			assertTrue(m.isFactorOld(factors[i]));
+		}
+	}
+	
+	public void testCheckFactorNonPresenceRandom() {
+		int n = r.nextInt(100) + 1;
+		String factors[] = new String[n];
+		TreeSet<String> set = new TreeSet<String>();
+		for (int i = 0; i < n; i++) {
+			int m = r.nextInt(100) + 1;
+			StringBuilder factor = new StringBuilder();
+			for (int j = 0; j < m; j++) 
+				factor.append((char)('a' + r.nextInt(26)));
+			factors[i] = factor.toString();
+			set.add(factors[i]);
+			this.m.checkFactor(factors[i]);
+		}
+		n = r.nextInt(200) + 1;
+		for (int i = 0; i < n; i++) {
+			int m = r.nextInt(100) + 1;
+			StringBuilder factor = new StringBuilder();
+			for (int j = 0; j < m; j++) 
+				factor.append((char)('a' + r.nextInt(26)));
+			String sfactor = factor.toString();
+			assertEquals(this.m.isFactorOld(sfactor), set.contains(sfactor));
 		}
 	}
 	
