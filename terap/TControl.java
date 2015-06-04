f2f4e23ee35a50453890f8b530db5cebb22393ca
Vbifial <vbifial@gmail.com>
2015-06-04 19:53:41 +0600

    Added class files with prototype methods.

---
+++ terap/TControl.java
@@ -0,0 +1,41 @@
+package terap;
+
+public class TControl {
+	
+	public void addRecord(String desease, String[] factors) {
+		
+	}
+	
+	public String getNextFactor() {
+		
+		return null;
+	}
+	
+	public void applyAnswer(boolean ans) {
+		
+	}
+	
+	public void checkFactor(String factor) {
+		
+	}
+	
+	public boolean isFactorOld(String factor) {
+		
+		return false;
+	}
+	
+	public int matchesCount() {
+		
+		return 0;
+	}
+	
+	public String match() {
+		
+		return null;
+	}
+	
+	public void clear() {
+		
+	}
+	
+}
